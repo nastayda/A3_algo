0cb5d4b2d8af886806cc09a54630a8333eb6bd1f
nastyada <nastyadanilkina1294@yandex.ru>
2017-11-16 10:58:23 +0300

    Determinate which rows are needed to be add to T_min

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,23 +13,54 @@ public class Main {
                 { 0, 1, 1, 1, 1 },
                 { 1, 0, 1, 1, 1 }
         };
+        //Определить какие стркои надо добавить в T_min
         ArrayList<Integer> T_min = new ArrayList<>( );
         for (int i = 0; i < 5; i++) {
-            for (int k = 1; k < 5; k++) {
+            for (int k = i+1; k < 6; k++) {
                 for (int j = 0; j < 5; j++) {
-                    if (T[ i ][ j ] <= T[ k ][ j ]) {
-                        if (j == 4) {
-                            T_min.add( i );
-                        }
-                    } else if (k != 4) {
-                        k++;
-                        if (j != 0) {
-                            j = 0;
+                    if (i != k) {
+                        if (T[ i ][ j ] <= T[ k ][ j ]) {
+                            if (j == 4) {
+                                if (!T_min.contains( i )){
+                                T_min.add( i );
+                                }
+                            }
+                        } else if (k < 6) {
+                            if (k < 5) {
+                                k++;
+                            }
+                            if (k == 5) {
+                                break;
+                            }
+                            if (j != 0) {
+                                j = 0;
+                            }
                         }
                     }
                 }
             }
         }
+       /* int k = 1;
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (T[ i ][ j ] <= T[ i+k ][ j ]){
+                    if (j==4){
+                    T_min.add( Integer.toString( i ) + " " + Integer.toString( i+1 ) );
+                    }
+                }
+                else if (k+1 < 6) {
+                    if (k < 4) {
+                        k++;
+                    }
+                    if (i == 5) {
+                        break;
+                    }
+                    if (j != 0) {
+                        j = 0;
+                    }
+                }
+            }
+        }*/
 
         for (int item : T_min) {
             System.out.println( T_min );
