401e3e2da3f340505844aad9c1e83c48bc1f9df9
nastyada <nastyadanilkina1294@yandex.ru>
2017-11-16 14:20:01 +0300

    Add rows to tMinMatrix

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,6 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -18,16 +15,16 @@ public class Main {
                 { 0, 1, 1, 1, 1 },
                 { 1, 0, 1, 1, 1 }
         };
-        //Определить какие стркои надо добавить в T_min
-        ArrayList<Integer> T_min = new ArrayList<>( );
+        //Определить какие стркои надо добавить в tMinIndexRows
+        ArrayList<Integer> tMinIndexRows = new ArrayList<>( );
         for (int i = 0; i < 5; i++) {
             for (int k = i + 1; k < 6; k++) {
                 for (int j = 0; j < 5; j++) {
                     if (i != k) {
                         if (T[ i ][ j ] <= T[ k ][ j ]) {
                             if (j == 4) {
-                                if (!T_min.contains( i )) {
-                                    T_min.add( i );
+                                if (!tMinIndexRows.contains( i )) {
+                                    tMinIndexRows.add( i );
                                 }
                             }
                         } else if (k < 6) {
@@ -46,23 +43,52 @@ public class Main {
             }
         }
 
+        //Добавить строки в tMinMatrix
+        int[][] tMinMatrix = new int[ tMinIndexRows.size( ) ][ T[ 0 ].length ];
+        for (int i = 0; i < tMinIndexRows.size( ); i++) {
+            for (int j = 0; j < T[ i ].length; j++) {
+                tMinMatrix[i][j]=T[tMinIndexRows.get( i )][j];
+            }
+        }
+        /*
+        for (int i = 0; i < tMinIndexRows.size( ); i++) {
+            for (int j = 0; j < T[ i ].length; j++) {
+                System.out.print( tMinMatrix[i][j] +" ");
+            }
+            System.out.println(" " );
+        }*/
+
+        //Составление КНФ
+        for ( int i = 0; i < tMinMatrix.length; i++ ) {
+            for ( int k = 0; k < tMinMatrix[0].length; k++ ) {
+                for ( int h = 1; h >= 0; h-- ) {
+                    for ( int j = 0; j < 4; j++ ) {
+                        T[i][j] = ( T1[k][j] ^ T2[h][j] );//mod2
+                    }i++;
+                }
+            }
+        }
+
         List<String> Tt = new ArrayList<>( );
         Tt.add( "413" );
         Tt.add( "123" );
         Tt.add( "123" );
         Tt.add( "456" );
-        Set<String> s = new HashSet<>(Tt);
+        Set<String> s = new HashSet<>( Tt );
 
-        for (String item : s) {
-            System.out.println( item );
-        }
+
+
+        //Получение p
+        ArrayList<String> DNF = new ArrayList<>( );
+        DNF.add( "12" );
+
         //Tt.removeIf( p -> p.equals( " " ) );
        /* int k = 1;
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
                 if (T[ i ][ j ] <= T[ i+k ][ j ]){
                     if (j==4){
-                    T_min.add( Integer.toString( i ) + " " + Integer.toString( i+1 ) );
+                    tMinIndexRows.add( Integer.toString( i ) + " " + Integer.toString( i+1 ) );
                     }
                 }
                 else if (k+1 < 6) {
