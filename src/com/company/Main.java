e003808316b8b39296a583e791e1a093f7e9ad90
nastayda <nastyadanilkina1294@yandex.ru>
2017-11-30 11:10:19 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,66 +9,133 @@ import java.util.stream.Collectors;
 public class Main {
 
     public static void main( String[] args ) {
-        int[][] T = {
-                { 1, 1, 0, 1, 0 },
-                { 0, 1, 1, 1, 0 },
-                { 1, 0, 1, 1, 0 },
-                { 1, 1, 0, 1, 1 },
-                { 0, 1, 1, 1, 1 },
-                { 1, 0, 1, 1, 1 }
-        };
+        int[][] T1 = { { 1, 1, 0, 1 },
+                { 0, 0, 0, 1 } };
+        int[][] T2 = { { 1, 0, 1, 0 },
+                { 0, 1, 1, 0 } };
+        int[] X = { 0, 1, 0, 1 };
+        System.out.println( "Универсальный алгоритм нахождения тупиковых тестов" );
+        System.out.println( "------------------" );
+        //Универсальный алгоритм нахождения тупиковых тестов
+        //ШАГ 1 - XOR------------------
+        int[][] T = sumByMod( T1, T2 );
+        //ШАГ 2 - MIN------------------
         //Определяем строки для добавления
         ArrayList<Integer> tMinIndexRows = determinWhatRowsAddToMinMass( T );
         //Добавляем строки
         int[][] tMinMatrix = addRowsToMinMass( T, tMinIndexRows );
-        //Составление КНФ
-        //Предварительный массив КНФ
-        ArrayList<Integer> massKNF = generateMassWithKNF( tMinMatrix );
-        //Обработка массива КНФ
-        //Убрать повторяющиеся символы
-        ArrayList<Integer> massKNFDeleteDoublesInItems = new ArrayList<>( );
-        for (int i = 0; i < massKNF.size( ); i++) {
-            massKNFDeleteDoublesInItems.add( Integer.parseInt( deleteDoublesSymbolsSec( massKNF.get( i ).toString( ) ) ) );
+        //ШАГ 3 - ДНФ------------------
+        //Предварительный массив ДНФ
+        ArrayList<Integer> massDNF = generateMassWithDNF( tMinMatrix );
+        //Обработка массива ДНФ
+        //Убрать повторяющиеся символы в элементах листа
+        ArrayList<Integer> massDNFDeleteDoublesInItems = new ArrayList<>( );
+        for (int i = 0; i < massDNF.size( ); i++) {
+            massDNFDeleteDoublesInItems.add( Integer.parseInt( deleteDoubleSymbolsInElements( massDNF.get( i ).toString( ) ) ) );
         }
-        //Убрать дубликаты
-        ArrayList<Integer> massKNFPreResult = deleteDuplicatesFromKNF( massKNFDeleteDoublesInItems );
-        ArrayList<String> mKNFString = new ArrayList<>( );
-        for (int item : massKNFPreResult) {
-            mKNFString.add( Integer.toString( item ) );
+        //Убрать дубликаты в листе
+        ArrayList<Integer> massDNFPreResult = deleteDuplicatesFromDNFList( massDNFDeleteDoublesInItems );
+        ArrayList<String> mDNFString = new ArrayList<>( );
+        for (int item : massDNFPreResult) {
+            mDNFString.add( Integer.toString( item ) );
         }
-        deleteSameKNFElements( mKNFString );
-
+        deleteSameDNFElements( mDNFString );
+        //АЛГОРИТМ А3------------------
         //Для подсчета весов
-        String KNFstring = StringUtils.join( mKNFString, "" );
-        System.out.println( KNFstring );
-        List<Character> textInChar = KNFstring.chars( ).mapToObj( e -> (char) e ).collect( Collectors.toList( ) );
-        Map<Character, Integer> countChar = textInChar.stream( ).collect( HashMap::new, ( m, c ) -> {
+        Map<Character, Integer> countChar = getCharacterIntegerMap( mDNFString );
+        //Посчитать веса
+        ArrayList<Double> p = countWeghts( countChar );
+
+        double z = 0.0;
+        z = getZ( T1, T2, p );
+        int k = 0;
+        //Инвертируем столбцы матрицы пока z не будет больше 0
+        while (z < 0) {
+            System.out.println( "Инвертируем столбцы матрицы пока z не будет больше 0" );
+            for (int i = 0; i < T1.length; i++) {
+                if (T1[ i ][ k ] == 0) {
+                    T1[ i ][ k ] = 1;
+                } else T1[ i ][ k ] = 0;
+            }
+            z = getZ( T1, T2, p );
+            k++;
+        }
+        // double h = 0.5*(findMaxMin( AlphaMin, "max" )-findMaxMin( Alphas, "min" ));
+        double h = 0.5 * z;
+        double xp = 0;
+        for (int i = 0; i < p.size( ); i++) {
+            xp += p.get( i ) * X[ i ];
+        }
+
+    }
+
+    public static double getZ( int[][] t1, int[][] t2, ArrayList<Double> p ) {
+        double z;
+        double[] AlphasT1 = getAlphas( p, t1 );
+        double[] AlphasT2 = getAlphas( p, t2 );
+        double AlphaMax = findMaxMin( AlphasT2, "max" );
+        double AlphaMin = findMaxMin( AlphasT1, "min" );
+        System.out.printf( "Вычисляем AlphaMin: %s и AlphaMax: %s", AlphaMin, AlphaMax );
+        System.out.println( );
+        System.out.println( "------------------" );
+        z = AlphaMin - AlphaMax;
+        System.out.printf( "Вычисляем z: %s", z );
+        System.out.println( );
+        System.out.println( "------------------" );
+        return z;
+    }
+
+    public static ArrayList<Double> countWeghts( Map<Character, Integer> countChar ) {
+        ArrayList<Double> p = new ArrayList<>( );
+        for (Integer item : countChar.values( )) {
+            p.add( (double) item / ( countChar.values( ).size( ) ) );
+        }
+        System.out.println( "Веса" );
+        System.out.println( "------------------" );
+        for (Double item : p) {
+            System.out.print( item + " " );
+        }
+        System.out.println( );
+        System.out.println( "------------------" );
+        return p;
+    }
+
+    public static Map<Character, Integer> getCharacterIntegerMap( ArrayList<String> mDNFString ) {
+        String DNFstring = StringUtils.join( mDNFString, "" );
+        List<Character> textInChar = DNFstring.chars( ).mapToObj( e -> (char) e ).collect( Collectors.toList( ) );
+        return textInChar.stream( ).collect( HashMap::new, ( m, c ) -> {
             if (m.containsKey( c ))
                 m.put( c, m.get( c ) + 1 );
             else
                 m.put( c, 1 );
         }, HashMap::putAll );
-        ArrayList<Double> p = new ArrayList<Double>( );
-        for (Integer item : countChar.values( )) {
-            //p.add( (double) item / ( countChar.values( ).size( ) ) );
-            p.add( 1.0 / 3 );
+    }
+
+    public static int[][] sumByMod( int[][] t1, int[][] t2 ) {
+        int[][] t = new int[ 4 ][ 4 ];
+        for (int i = 0; i < t.length; i++) {
+            for (int k = 0; k < 2; k++) {
+                for (int h = 1; h >= 0; h--) {
+                    for (int j = 0; j < t[ 0 ].length; j++) {
+                        t[ i ][ j ] = ( t1[ k ][ j ] ^ t2[ h ][ j ] );//mod2
+                    }
+                    i++;
+                }
+            }
         }
+        System.out.println( "ШАГ 1 - XOR" );
+        System.out.println( "-----------------" );
+        printMatrixInt( t );
+        System.out.println( "-----------------" );
+        return t;
+    }
 
-        int[] X = { 1, 1, 1, 0, 1 };
-        int[][] T1 = {
-                { 1, 1, 0, 1 },
-                { 0, 0, 0, 1 },
-                { 1, 0, 1, 1 }
-        };
-        double[] Alphas = getAlphas( p, T1 );
-
-        double AlphaMax = findMaxMin( Alphas, "max" );
-        double AlphaMin = findMaxMin( Alphas, "min" );
-
-        double z = AlphaMin - AlphaMax;
-        
-        if (z<0){
-            //Инвертируем
+    public static void printMatrixInt( int[][] t ) {
+        for (int h = 0; h < t.length; h++) {
+            for (int j = 0; j < t[ 0 ].length; j++) {
+                System.out.print( t[ h ][ j ] + " " );
+            }
+            System.out.println( );
         }
     }
 
@@ -131,7 +198,7 @@ public class Main {
         return massKNFResult;
     }
 
-    private static ArrayList<Integer> deleteDuplicatesFromKNF( ArrayList<Integer> massKNFDeleteDoublesInItems ) {
+    private static ArrayList<Integer> deleteDuplicatesFromDNFList( ArrayList<Integer> massKNFDeleteDoublesInItems ) {
         ArrayList<Integer> l1 = new ArrayList( massKNFDeleteDoublesInItems );
         ArrayList<Integer> l2 = new ArrayList( );
         Iterator iterator = l1.iterator( );
@@ -156,7 +223,7 @@ public class Main {
         System.out.println( );
     }
 
-    private static String deleteDoublesSymbolsSec( String string ) {
+    private static String deleteDoubleSymbolsInElements( String string ) {
         Set<Character> chars = new HashSet( );
         for (int i = 0; i < string.length( ); i++) {
             chars.add( string.charAt( i ) );
@@ -185,14 +252,15 @@ public class Main {
         System.out.println( str.replaceAll( "(?i)[" + duplicates + "]", "" ) );
     }
 
-    private static void deleteSameKNFElements( List<String> massKNF ) {
+    private static void deleteSameDNFElements( ArrayList<String> massKNF ) {
         for (int j = 0; j < massKNF.size( ); j++) {
             String item = massKNF.get( j );
             massKNF.removeIf( p -> p.contains( item.toString( ) ) && p.length( ) > item.toString( ).length( ) );
         }
-        for (String key : massKNF) {
-            System.out.print( key + " " );
-        }
+        System.out.println( "Тупиковые тесты" );
+        System.out.println( "-----------------" );
+        printMassString( massKNF );
+        System.out.println( "-----------------" );
     }
 
     private static int[][] addRowsToMinMass( int[][] t, ArrayList<Integer> tMinIndexRows ) {
@@ -203,27 +271,31 @@ public class Main {
                 tMinMatrix[ i ][ j ] = t[ tMinIndexRows.get( i ) ][ j ];
             }
         }
+        System.out.println( "ШАГ 2 - MIN" );
+        System.out.println( "-----------------" );
+        printMatrixInt( tMinMatrix );
+        System.out.println( "-----------------" );
         return tMinMatrix;
     }
 
     private static ArrayList<Integer> determinWhatRowsAddToMinMass( int[][] t ) {
         //Определить какие стркои надо добавить в tMinIndexRows
         ArrayList<Integer> tMinIndexRows = new ArrayList<>( );
-        for (int i = 0; i < 5; i++) {
-            for (int k = i + 1; k < 6; k++) {
-                for (int j = 0; j < 5; j++) {
+        for (int i = 0; i < t.length; i++) {
+            for (int k = i + 1; k < t[ 0 ].length; k++) {
+                for (int j = 0; j < t.length; j++) {
                     if (i != k) {
                         if (t[ i ][ j ] <= t[ k ][ j ]) {
-                            if (j == 4) {
+                            if (j == t.length - 1) {
                                 if (!tMinIndexRows.contains( i )) {
                                     tMinIndexRows.add( i );
                                 }
                             }
-                        } else if (k < 6) {
-                            if (k < 5) {
+                        } else if (k < t[ 0 ].length) {
+                            if (k < t.length) {
                                 k++;
                             }
-                            if (k == 5) {
+                            if (k == t.length) {
                                 break;
                             }
                             if (j != 0) {
@@ -237,7 +309,7 @@ public class Main {
         return tMinIndexRows;
     }
 
-    private static ArrayList<Integer> generateMassWithKNF( int[][] tMinMatrix ) {
+    private static ArrayList<Integer> generateMassWithDNF( int[][] tMinMatrix ) {
         //Присвоить первую строку КНФ массиву
         ArrayList<Integer> testRowFirst = new ArrayList<>( );
         for (int i = 0; i < tMinMatrix[ 0 ].length; i++) {
